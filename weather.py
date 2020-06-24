54cec4b52eac938ef98cadc27fed787da39b3cd0
Gowrisankar JG <43264841+GowriSankar-JG@users.noreply.github.com>
2020-06-24 15:36:41 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,18 +1,9 @@
 import requests
 def Weather(city):
-    API_key = "87bc2fcd3bb7cee050c3b5907e0f5c61"
+    API_key = "Your API Key here"
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
-    #loc = tracker.get_slot('location')
-    #loc=input('location: ')
     
     Final_url = base_url + "appid=" + API_key + "&q=" + city + "&units=metric"
     weather_data = requests.get(Final_url).json()
-    
-    # if weather_data:
-    #     temp = weather_data['main']['temp']
-    #     #description = weather_data['weather'][0]['description']
-    #     output="The temperature of {} is {} °C.".format(city,temp)
-    # else:
-    #     output="We did not find any weather information for {}. Try by a city name.".format(city)
     
     return weather_data['main']
